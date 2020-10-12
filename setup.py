062a103e08281a0b416a03cf1bcd2f8228a65f23
Lazlo Westerhof <l.r.westerhof@uu.nl>
2020-10-12 09:32:56 +0200

    Add Python 3.9 to supported Python versions.

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     python_requires='>=3.6',
     project_urls={
