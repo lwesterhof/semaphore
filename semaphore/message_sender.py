6d419301d61e2a10ee652dcb7ea8fa18bb423a24
Soenke Huster <me@eknoes.de>
2021-05-15 17:45:30 +0200

    Fix not returning False

--- semaphore/message_sender.py
+++ semaphore/message_sender.py
@@ -72,7 +72,7 @@ class MessageSender:
             if results:
                 if results[0].get('success'):
                     return True
-        return False
+            return False
 
     async def send_message(self, receiver, body, attachments=None) -> bool:
         """
