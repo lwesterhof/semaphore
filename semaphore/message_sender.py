984c4175649ed7048b5d3a39d851dfd133a8f16c
Lazlo Westerhof <l.r.westerhof@uu.nl>
2021-06-24 17:43:14 +0200

    Fix missing return statement.

--- semaphore/message_sender.py
+++ semaphore/message_sender.py
@@ -81,6 +81,7 @@ class MessageSender:
                     if results[0].get('success'):
                         return True
                 return False
+            return False
 
     async def send_message(self, receiver, body, attachments=None) -> bool:
         """
