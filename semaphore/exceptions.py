44df56c6e15eab2854c03463bf867d61a35f7520
Soenke Huster <soenke.huster@eknoes.de>
2021-11-29 15:21:59 +0100

    Fix import order

--- semaphore/exceptions.py
+++ semaphore/exceptions.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """This module contains exceptions for Semaphore."""
-from typing import List, Dict, Optional
+from typing import Dict, List, Optional
 
 
 class StopPropagation(Exception):
